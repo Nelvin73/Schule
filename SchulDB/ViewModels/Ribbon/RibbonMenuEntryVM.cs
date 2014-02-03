f34d77000163f471841b54c769d3dfc4043c998d
Nelvin73 <Christian.Groll@gmail.com>
2014-02-03 15:26:01 +0100

    added

---
+++ SchulDB/ViewModels/Ribbon/RibbonMenuEntryVM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Groll.Schule.SchulDB.Helper;
+using Groll.Schule.DataManager;
+using Groll.Schule.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+using System.Windows.Input;
+
+
+namespace Groll.Schule.SchulDB.ViewModels
+{
+    public class RibbonMenuEntryVM : RibbonBaseVM
+    {        
+ 
+        private string longHeader = "";
+        private List<RibbonMenuEntryVM> itemsSource;
+        private RibbonMenuEntryVM selectedItem;
+
+        /// <summary>
+        /// Längerer Text, z.B. für Gallery
+        /// </summary>
+        public string LongHeader
+        {
+            get { return longHeader; }
+            set { longHeader = value; OnPropertyChanged(); }
+        }
+
+        
+        public List<RibbonMenuEntryVM> ItemsSource
+        {
+            get { return itemsSource; }
+            set { itemsSource = value; }
+        }
+
+        public RibbonMenuEntryVM SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value; OnPropertyChanged();
+                }
+            }
+        }
+      
+    }
+}
