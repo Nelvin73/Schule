f6665854d63d964313f9c7c298fae274ba9a7f3d
Nelvin73 <Christian.Groll@gmail.com>
2014-05-30 17:44:46 +0200

    123

---
+++ SchulDB/ViewModels/Ribbon/Tabs/RibbonTabStandardVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Groll.Schule.SchulDB.Helper;
+using Groll.Schule.DataManager;
+using Groll.Schule.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Media;
+using Groll.Schule.SchulDB.Commands;
+
+
+namespace Groll.Schule.SchulDB.ViewModels
+{
+    public class RibbonTabStandardVM : RibbonTabViewModel
+    {      
+        #region Properties für Bindings
+     
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>        
+        public RibbonTabStandardVM()
+        {
+            Label = "Standard";
+            IsSelected = true;                             
+        }
+        #endregion
+
+       
+    }
+}
